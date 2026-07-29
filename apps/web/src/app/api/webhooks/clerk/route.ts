34ab8eaab96dfb8d940c745d6f576dd542229ba9
JXDeveloper <shurahbil.abd@gmail.com>
2026-07-29 10:27:20 +0500

    added a test wehook for user created

---
+++ apps/web/src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const payload = await request.json();
+
+    console.log("-------- Webhook Payload Received--------");
+    console.log(JSON.stringify(payload, null, 2));
+
+    // Respond with a 200 OK status to acknowledge receipt of the webhook
+    return NextResponse.json({ message: "Webhook received successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    return NextResponse.json({ message: "Error processing webhook" }, { status: 500 });
+  }
+}
