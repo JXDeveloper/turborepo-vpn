90f48fdefda40673b5c5a4b004f5e506090192cd
JXDeveloper <shurahbil.abd@gmail.com>
2026-09-05 17:50:38 +0500

    removed unecessary things from __root.tsx and main page only contains buttons for sign in sign up and reroute to dashboard/if already signed in and removed connect logic from main file

--- apps/desktop/src/renderer/src/routes/__root.tsx
+++ apps/desktop/src/renderer/src/routes/__root.tsx
@@ -1,23 +1,8 @@
-import { Show, UserButton } from '@clerk/electron/react'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
-      <Show when="signed-out">
-        <Link to="/sign-in" className="[&.active]:font-bold">
-          Sign In
-        </Link>
-      </Show>
-      <Show when="signed-in">
-        <UserButton />
-      </Show>
-    </div>
-    <hr />
     <Outlet />
     <TanStackRouterDevtools />
   </>
